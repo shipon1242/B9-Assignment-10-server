6dc4cc546e6be25acf4530850ef54639b596a008
shipon1242 <hossainmohammadshipon@gamil.com>
2024-06-16 16:48:22 +0600

    crafts view details functionality added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5001
@@ -56,7 +56,12 @@ async function run() {
       res.send(result)
 
 
-
+    })
+    app.get('/crafts/:id',async(req,res)=>{
+      const id =req.params.id
+      const query ={_id: new ObjectId(id)}
+      const result =await craftCollection.findOne(query)
+      res.send(result)
     })
 
 
